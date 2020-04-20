55e63afdad8a9f67c38fb4dd6cf0090fbc4bf0f8
tomek <tomeksochacki@wp.pl>
2020-04-20 10:51:30 +0200

    Update

--- src/com/example/foreach/collectiontest/New.java
+++ src/com/example/foreach/collectiontest/New.java
@@ -1,7 +1,9 @@
 package com.example.foreach.collectiontest;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class New {
     public static void main(String[] args) {
@@ -22,7 +24,23 @@ public class New {
             System.out.println("Numbers even in list are: " + numbersEvenListN);
         }
 
+        Set<Integer> numberSet = new HashSet<>();
+        System.out.println("What is size set numberSet? Size is = " + numberSet.size());
+        numberSet.add(1);
+        numberSet.add(2);
+        numberSet.add(5);
+        numberSet.add(5);
+        System.out.println("What is size set numberSet? Size is = " + numberSet.size());
+        System.out.println("Is set numberSet empty? " + numberSet.isEmpty());
+        System.out.println("Set numberSet contains variable number 2? " + numberSet.contains(2));
+        System.out.println("Set numberSet contains variable number 8? " + numberSet.contains(8));
 
+        for (Integer numberSetS: numberSet) {
+            System.out.println("Variables number in set are: " + numberSetS);
+        }
+        for (Integer numberSetS: numberSet) {
+            System.out.println("Variables number in set are: " + numberSetS);
+        }
 
     }
 }
